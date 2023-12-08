b21d01e36d078774b39f14ffc29b005acde00206
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2023-12-08 17:25:38 -0300

    adicionando ssl true na configuração do banco

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const pool = new Pool ({
     host: 'dpg-clpikt1oh6hc73c4eef0-a.oregon-postgres.render.com',
     database: 'awesomechat_jdbs',
     password: 'HnFzb5FG2BRyGNPZo4TETjWrjPSI7I80',
-    port: 5432
+    port: 5432,
+    sll: true
 });
 
 module.exports = pool;
