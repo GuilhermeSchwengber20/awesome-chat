2335bea27d726db7d5b78e169ee3219e3a4c9e34
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2023-12-08 17:32:52 -0300

    corrigindo o caminho da api

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3030',
+    baseURL: 'https://awesomechat-api.onrender.com/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
