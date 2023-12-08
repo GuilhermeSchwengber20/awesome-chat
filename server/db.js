93bd9a873a7717a9c71634737720ed39ca430cfb
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2023-12-08 12:01:05 -0300

    mudando configurações de banco de dados

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg")
 
 
 const pool = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'awesomechat',
-    password: 'postgres',
+    user: 'guilas',
+    host: 'dpg-clpikt1oh6hc73c4eef0-a.oregon-postgres.render.com',
+    database: 'awesomechat_jdbs',
+    password: 'HnFzb5FG2BRyGNPZo4TETjWrjPSI7I80',
     port: 5432
 });
 
