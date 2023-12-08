b0810c585d5c7f05eeff454fdb80e42d759f63dc
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2023-12-08 11:26:45 -0300

    mudando host e adicionando porta

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -17,6 +17,11 @@ export default {
     ]
   },
 
+  server: {
+    host: '0.0.0.0',
+    port: '10000'
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~/assets/main.css",
