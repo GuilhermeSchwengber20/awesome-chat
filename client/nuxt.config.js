def13bc6b28281d7b170b2a97395d4d5d2f70b59
GuilhermeSchwengber20 <guilhermenft2021@gmail.com>
2024-01-01 19:04:02 -0300

    production config

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -42,14 +42,14 @@ export default {
     sockets: [
       {
         name: "chat",
-        url: "http://localhost:3030",
+        url: "https://awesomechat-api.onrender.com/",
       },
     ],
   },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://localhost:3030",
+    baseURL: "https://awesomechat-api.onrender.com/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- client/plugins/socket-io.js
+++ client/plugins/socket-io.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3030");
+const socket = io("https://awesomechat-api.onrender.com/");
 
 Vue.prototype.$socket = socket;
